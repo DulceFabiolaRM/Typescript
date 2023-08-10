5a49972ee2cad5e3a5acff43a35889193e242e71
Dulce Fabiola <104787408+DulceFabiolaRM@users.noreply.github.com>
2023-08-10 14:46:06 -0600

    fix: update
    test

--- Bases/funciones/functions.ts
+++ Bases/funciones/functions.ts
@@ -1,15 +1,15 @@
 (()=>{
-    const hero:string="Flash"
+    const hero:string="Batman"
 
     function returnName():string{
         return hero
     }
 
-    const activarBatiseñal=():string=>{
+    const activarBS=():string=>{
         return "Batiseñal activa";
     }
  
-    console.log(typeof activarBatiseñal)
+    console.log(typeof activarBS)
 
     const heroName=returnName()
 })()
